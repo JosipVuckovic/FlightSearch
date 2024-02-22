309013fd000f216994d0352e60fa5a8161c8ade5
Josip Vuckovic <95757369+VuckovicJosip@users.noreply.github.com>
2024-02-22 18:40:28 +0100

    ADD/DEL: - removed scaffolded files, added new FlightSearchController

---
+++ FlightSearch.Server/Controllers/FlightSearchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightSearch.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FlightSearchController : ControllerBase
+{
+    public FlightSearchController()
+    {
+        
+    }
+
+    [HttpGet(Name = "GetFlightSearch")]
+    public IActionResult Get()
+    {
+        throw new NotImplementedException();
+    }
+}
+
