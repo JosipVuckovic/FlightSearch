3b6f83b98ec1166f7a3909c31f305a62559d03c7
Josip Vuckovic <95757369+VuckovicJosip@users.noreply.github.com>
2024-02-27 23:01:31 +0100

    UPD: - added settings check at db init, if the db not used do not init it

--- FlightSearch.Data/DbInitializer.cs
+++ FlightSearch.Data/DbInitializer.cs
@@ -1,19 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FlightSearch.Data.Models.Config;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace FlightSearch.Data;
 
 public class DbInitializer
 {
     private readonly FlightSearchApplicationDb _context;
+    private readonly CacheAndDatabaseSettings? _settings;
 
     public DbInitializer(IServiceScope scope)
     {
         _context = scope.ServiceProvider.GetService<FlightSearchApplicationDb>();
+        _settings = scope.ServiceProvider.GetService<IOptions<CacheAndDatabaseSettings>>()?.Value;
     }
 
     public async Task InitAsync()
     {
+        if (_settings is not {ShouldUseDatabase: true, DatabaseNotOlderThenMinutes: not null})
+            return;
+        
         await _context.Database.MigrateAsync();
     }
 }
